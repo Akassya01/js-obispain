3bc8cb77bca09840d15099afc3a6b7921ee0f187
Akassya01 <00001106094372sp@al.educacao.sp.gov.br>
2024-08-09 10:33:15 -0300

    Adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {
+  
+    stroke("black");
+    fill("black");
+  
+    // console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 10, 25);
+    }
+  }
+  
