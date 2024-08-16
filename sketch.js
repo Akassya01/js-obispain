3d1b56ee51037a506ac9c8565d4d7358906a9b28
Akassya01 <00001106094372sp@al.educacao.sp.gov.br>
2024-08-16 10:40:14 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 function setup() {
     createCanvas(600, 600);
-    background("white");
+    background("black");
   }
   
   function draw() {
   
-    stroke("black");
-    fill("black");
+    stroke("pink");
+    fill("pink");
   
     // console.log(mouseIsPressed);
   
